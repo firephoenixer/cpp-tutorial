19a8568b6ccdf00b5a653e511c586ebec614774a
firephoenixer <owen00721@gmail.com>
2022-11-16 15:58:48 +0800

    create tools functions

--- practice.cpp
+++ practice.cpp
@@ -1,10 +1,12 @@
 ﻿// this is main function.
 // This file manage the testing of every syntax.
 #include <iostream>
+#include "tools.h"
 
 int main()
 {
     std::cout << "Beginning of a test..." << std::endl;
+    helloWorld();
     std::cout << "Ending of a test..." << std::endl;
     return 0;
 }
---
+++ tools.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+// creat a test function, show how to organize functions in c++
+void helloWorld()
+{
+	std::cout << "Hello world!" << std::endl;
+	return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
