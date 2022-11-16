8891c041bd11452d91b373fb1e4da5e24728d1d8
firephoenixer <owen00721@gmail.com>
2022-11-16 15:53:49 +0800

    Main

--- practice.cpp
+++ practice.cpp
@@ -1,11 +1,11 @@
-﻿
-//
-
+﻿// this is main function.
+// This file manage the testing of every syntax.
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World! \n";
+    std::cout << "Beginning of a test..." << std::endl;
+    std::cout << "Ending of a test..." << std::endl;
     return 0;
 }
 
